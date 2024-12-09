91ef146bd51eab58bdeb948cf71447d716591486
mariaspyrou <maria.spyrou@ifu.uni-tuebingen.de>
2024-12-09 20:24:59 +0100

    Update Heatmap-virulence-treponema.R

--- virulence-gene-analysis/Heatmap-virulence-treponema.R
+++ virulence-gene-analysis/Heatmap-virulence-treponema.R
@@ -1,11 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2759
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww25780\viewh9700\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 library(ggplot2)\
+library(ggplot2)\
 heatmap_data <- read.csv('Gene-coverages-heatmap-treponema.csv')\
 colnames(heatmap_data)<- c('Genome','Coverage','Genes')\
 heatmap_data$Genome <- factor(heatmap_data$Genome, levels = rev(unique(heatmap_data$Genome)), ordered=TRUE)\
